2a27810d77a82bbe45094ec1d9f9909ebc3d9b79
aniikaa05 <aniikaa.4044@gmail.com>
2026-04-17 02:34:45 +0600

    name final

--- Final/Lab1/Name/B/name.php
+++ Final/Lab1/Name/B/name.php
@@ -1,14 +1,21 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['userName'];
+
+    if ($username == "") {
+        echo "Invalid Username";
+    } else {
+        echo "Name: " . $username;
+    }
+}
+
+?>
 <form action="" method="post">
     <fieldset>
         <legend>NAME</legend>
         <input type="text" name="userName">
         <hr>
-        <input type="submit" value="Submit">
-</fieldset>
+        <input type="submit" name="submit" value="Submit">
+    </fieldset>
 </form>
-<?php
-if(isset($_POST['userName'])) {
-    $name = $_POST['userName'];
-    echo "Name: ". $name;
-}
-?>
--- Final/Lab1/Name/C/name.php
+++ Final/Lab1/Name/C/name.php
@@ -1,19 +1,21 @@
 <?php
-$name = "";
-if (isset($_POST['userName'])) {
-    $name = $_POST['userName'];
-}
+$username = "";
+if (isset($_POST['submit'])) {
+    $username = $_POST['userName'];
+
+    if ($username == "") {
+        echo "Invalid Name";
+    } else {
+        echo "Name: " . $username;
+    }
+} 
 ?>
+
 <form action="" method="post">
     <fieldset>
         <legend>NAME</legend>
-        <input type="text" name="userName" value="<?php echo $name; ?>"> 
+        <input type="text" name="userName" value="<?php echo $username; ?>"> 
         <hr>
-        <input type="submit" value="Submit">
+        <input type="submit" name="submit" value="Submit">
     </fieldset>
 </form>
-<?php 
-if ($name != "") {
-    echo "Output: " . $name;
-}
-?>
