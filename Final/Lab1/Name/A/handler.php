d5857080b666039805caab2a8753bcb08b9d3689
aniikaa05 <aniikaa.4044@gmail.com>
2026-04-17 02:19:28 +0600

    final name

--- Final/Lab1/Name/A/handler.php
+++ Final/Lab1/Name/A/handler.php
@@ -1,4 +1,14 @@
 <?php
-$name = $_POST['userName'];
-echo "Name: " . $name;
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['userName'];
+    if ($username == "") {
+        echo "Invalid Username";
+    } else {
+        echo "Name: " . $username;
+    }
+} else {
+    echo "Form not submitted properly";
+}
+
 ?>
--- Final/Lab1/Name/A/name.php
+++ Final/Lab1/Name/A/name.php
@@ -1,8 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Enter Name</title>
+</head>
+<body>
+
 <form action="handler.php" method="post">
     <fieldset>
         <legend>NAME</legend>
         <input type="text" name="userName"> 
         <hr>
-        <input type="submit" value="Submit">
+        <input type="submit" name="submit" value="Submit">
     </fieldset>
 </form>
+
+</body>
+</html>
